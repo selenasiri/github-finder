d8e5c1b9f687a7000dabe8a8bb0ac60d4a7bfc04
Selena Siri <selenasiri@gmail.com>
2019-12-22 22:36:22 -0500

    added react router

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// rafce
+const About = () => {
+  return (
+    <>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+    </>
+  );
+};
+
+export default About;
