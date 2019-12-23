6a3cb0450fecf0ac4e0d968f993c911e62fd19bd
Selena Siri <selenasiri@gmail.com>
2019-12-22 22:04:43 -0500

    added alert for empty search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -11,15 +11,21 @@ class Search extends Component {
     // ptfr
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,7 +13,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <a href={html_url} className='btn btn-dark btn-sm my-1' target='_blank'>
           More
         </a>
       </div>
