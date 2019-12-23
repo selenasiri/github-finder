d4f8feee0cf31af61aa50de47d7af09323165395
Selena Siri <selenasiri@gmail.com>
2019-12-23 00:08:18 -0500

    typo

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -100,7 +100,7 @@ class User extends Component {
         <div className='card text-center'>
           <div className='badge badge-primary'>Followers: {followers}</div>
           <div className='badge badge-success'>Following: {following}</div>
-          <div className='badge badge-white'>PUblic Repos: {public_repos}</div>
+          <div className='badge badge-white'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
       </>
