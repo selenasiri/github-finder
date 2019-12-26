c02a8873399511d66ee0d00ee40f695f53f9a8b4
Selena Siri <selenasiri@gmail.com>
2019-12-26 14:09:23 -0500

    added NotFound page (component)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Alert from './components/layout/Alert';
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 
 import GithubState from './context/github/GithubState';
@@ -26,6 +27,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
                 />
               </Switch>
             </div>
