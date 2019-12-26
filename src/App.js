8f5c3230b987bd67d60f219f5185d9c7d190499e
Selena Siri <selenasiri@gmail.com>
2019-12-26 14:02:45 -0500

    added Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import User from './components/users/User';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -23,16 +23,7 @@ const App = () => {
               <Alert />
 
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
                 />
